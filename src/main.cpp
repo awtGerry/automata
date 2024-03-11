965a4af8790bb41550933a454ac1810121093f36
Victor Rodriguez <awtgerry@gmail.com>
2024-03-11 00:02:49 -0600

    add java gui
    TODO: do it with c++ later.

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,6 @@
-/*
-Automata  que  recibe  un  archivo  externo(Con  extensión  .txt), el  cual contendrá
-exclusivamente cadenas de texto (Texto plano), el objetivo del sistema es identificar en el
-escrito las coincidencias de las siguientes dos palabras: Web y Ebay
-*/
-
-/*
-La interfaz del sistema debe ser en un entorno visual, es decir, con ventanas, botones, cajas
-de texto, etiquetas, tablas, etc.
-*/
-
-/*
-∑  Representa cualquier carácter , por ejemplo la simbología ∑ -e -w para el estado 1 se
-interpreta que si ingresa cualquier caracter , excepto la e y la w se debe transitar al mismo
-estado. Si estoy en el estado 1 y se ingresa el caracter w se debe transitar al estado 12. Si
-estoy en el estado 1 y se ingresa el carácter e se debe de transitar al estado 15.
-
-Al final, el sistema deberá desplegar de forma visual cuantas coincidencias encontró de la
-palabra web y cuantas coincidencias  encontró de la palabra ebay.
-*/
-
-/*
-Consideraciones importantes:
-El conteo de las coincidencias para web y ebay se debe de realizar estrictamente
-a través del autómata, es decir, el sistema, una vez que reciba el archivo externo,
-deberá de recorrerlo caracter por caracter para identificar las coincidencias, cada
-caracter representa una entrada para el autómata.
-“Si  se  utiliza  una  expresión  regular  para  identificar  las  palabras  se  anula  el
-proyecto”
-Para que el sistema cuente una coincidencia, las palabras web y ebay deben de estar sin
-espacios  en  blanco entre  cada  caracter,  tampoco  debe  existir  saltos  de  línea  o
-tabuladores entre estos, por ejemplo, las siguientes palabras no deben ser contabilizadas
-por que tienen espacios en blanco, tabuladores y saltos de línea
-we b                             
-w    e    b
-e
-bay
-
-Cada vez que el sistema encuentre la palabra  webay  deberá de contabilizar 1 vez la
-palabra web y 1 vez la palabra ebay            webay        webay
-Es indistino el uso de mayúsculas y minúsculas, es decir, no importa si la palabra web
-aparece en mayúsculas, minúsculas o una combinación de estas, la palabra deberá de ser
-contabilizada.
-Nota: La implementación del autómata se debe de realizar desde cero, es
-decir, el alumno debe de codificar TODA la solución del sistema
-*/
-
-/* AUTOMATA */
-/*
-estado 1 ∑ -e-w
-estado 12: (∑ -e-w a estado 1) (w a estado 12) (e a estado 135)
-estado 135: (∑ -b-e-w a estado 1) (w a estado 12) (e a estado 15) (b a estado 146)
-estado 146: (∑ -a-e-w estado 1) (w a estado 12) (e a estado 15) (a a estado 17)
-estado 15: (∑ -b-e-w a estado 1) (e a estado 15) (w a estado 12) (b a estado 16)
-estado 16: (∑ -a-e-w a estado 1) (e a estado 15) (w a estado 12) (a a estado 17)
-estado 17: (∑ -e-w-y a estado 1) (e a estado 15) (w a estado 12) (y a estado 18)
-estado 18: (∑ -e-w a estado 1) (e a estado 15) (w a estado 12)
-*/
-
 #include <iostream>
 #include <string>
+#include <fstream>
 
 std::string word = "";
 int current_index = 0;
@@ -94,10 +36,28 @@ void get_web_12();
 void get_web_135();
 void get_web_146();
 
-/* MAIN */
 int main(int argc, char *argv[])
 {
-    word = "webay";
+    std::ifstream myfile("file.txt");
+    if (myfile.is_open())
+    {
+        std::string line;
+        while (getline(myfile, line))
+        {
+            word += line;
+        }
+        myfile.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file";
+        return 1;
+    }
+
+    for (int i = 0; i < word.length(); i++)
+    {
+        word[i] = tolower(word[i]);
+    }
     start_state();
     std::cout << "Ebay count: " << get_ebay_count() << std::endl;
     std::cout << "Web count: " << get_web_count() << std::endl;
---
+++ src/tool.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <imgui.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ImGui::Text("Hello, world!");
+    // if (ImGui::Button("Click me"))
+    // {
+    //     std::cout << "Button clicked" << std::endl;
+    // }
+    // ImGui::InputText("Input", &word);
+    // ImGui::SliderInt("Ebay count", &ebay_count, 0, 100);
+    return 0;
+}
