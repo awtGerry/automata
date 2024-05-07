a595b1220b901ab63cf2053389e4ae966962a650
Victor Rodriguez <awtgerry@gmail.com>
2024-05-06 21:19:08 -0600

    tui nfatodfa

--- runner.sh
+++ runner.sh
@@ -1,3 +1,15 @@
-echo "Starting automata..."
-
-java src/main.java
+case $1 in
+    1)
+        echo "Running automata"
+        javac -d bin src/main.java
+        java -cp bin main
+        ;;
+    2)
+        echo "Running NFA to DFA"
+        javac -d bin src/NFAtoDFA.java
+        java -cp bin NFAtoDFA
+        ;;
+    *)
+        echo "Invalid option"
+        ;;
+esac
